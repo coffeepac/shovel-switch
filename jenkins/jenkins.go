ed26c49c856b92842c1675929342750705f4b959
Pat Christopher <patrick.christopher@mtnsat.com>
2013-09-12 11:54:06 -0700

    intermediate commit, machine getting flaky

--- jenkins/jenkins.go
+++ jenkins/jenkins.go
@@ -1,16 +1,20 @@
 package jenkins
 
 import (
+  "time"
   "strconv"
   "errors"
-//  json "encoding/json"
+  json "encoding/json"
   http "net/http"
   httputil "net/http/httputil"
   url  "net/url"
 )
 
 var (
-  ciURL = "http://jenkins-cd.mtnsatcloud.com/job/promote-to-ship/buildWithParameters"
+  ciURL     = "http://jenkins-cd.mtnsatcloud.com/job/promote-to-ship/"
+  ciPostURL = ciURL + "buildWithParameters"
+  ciLastBuild = ciURL + "api/json?depth=1&tree=lastBuild[actions[parameters[name,value]]],url"
+  ciResult  = "api/json?tree=result"
 )
 
 /*
@@ -21,11 +25,33 @@ type PromoteToShip struct {
 }
 
 /*
+**  struct representation of the lastBuild json rest response
+*/
+type lastBuildResponse struct {
+    LastBuild struct {
+        Actions []struct {
+            Parameters []struct {
+                Name    string  `json:"name"`
+                Value   string  `json:"value"`
+            }   `json:"parameters"`
+        }   `json:"actions"`
+        Url string  `json:"url"`
+    }   `json:"lastBuild"`
+}
+
+/*
+**  struct rep for result resp response
+*/
+type jobResult struct {
+    Result  string  `json:"result"`
+}
+
+/*
 **  Start - queues up the promote to ship job with this Shipcode on jenkins-ci
 */
 func (p *PromoteToShip) Start() (err error) {
     //  post the job!
-    resp, err := http.PostForm(ciURL,url.Values{"SHIPNAME": {p.Shipcode}})
+    resp, err := http.PostForm(ciPostURL,url.Values{"SHIPNAME": {p.Shipcode}})
     if err != nil {
         return err
     } else if resp.StatusCode != 201 {
@@ -43,6 +69,62 @@ func (p *PromoteToShip) Start() (err error) {
 **         + has a result
 **           - result is a field in the json status
 **           - if there is a result, the job is done
+**
+**  TODO:  add a timeout so we don't loop forever
 */
-func (p *PromoteToShip) Wait() {
+func (p *PromoteToShip) Wait(sleepSeconds int) (err error) {
+    //  poll lastBuild until our job shows up there.
+    //  lastBuild shows current build, if there is one
+    jobNotStarted := true
+    var lastBuild lastBuildResponse
+    for jobNotStarted {
+        resp, err := http.Get(ciLastBuild)
+        if err != nil {
+            return err
+        }
+
+        decoder := json.NewDecoder(resp.Body)
+        err = decoder.Decode(&lastBuild)
+        resp.Body.Close()
+        if err != nil {
+            return err
+        }
+
+        if lastBuild.LastBuild.Actions.Parameters.Value == p.Shipcode {
+            jobNotStarted = false
+        }
+
+        time.Sleep(time.Duration(sleepSeconds) * time.Second)
+    }
+
+    //  have the job, poll the job url until a result is present
+    jobRunning := true
+    var result jobResult
+    for jobRunning {
+        resp, err := http.Get(lastBuild.LastBuild.Url + ciResult)
+        if err != nil {
+            return err
+        }
+
+        decoder := json.NewDecoder(resp.Body)
+        err = decoder.Decode(&result)
+        resp.Body.Close()
+        if err != nil {
+            return err
+        }
+
+        if result.Result != "" {
+            jobRunning = false
+        }
+
+        time.Sleep(time.Duration(sleepSeconds) * time.Second)
+    }
+
+    if result.Result != "SUCCESS" {
+        err = errors.New("Job did not succeed.  Result is: " + result.Result)
+    } else {
+        err = nil
+    }
+
+    return err
 }
--- jenkins/jenkins_test.go
+++ jenkins/jenkins_test.go
@@ -9,6 +9,9 @@ import (
 
 var (
     dummyJenkinsLatch = false
+    jobStarted = false
+    jobCompleted = false
+    shipcode string
 )
 
 func dummyJenkins(){
@@ -17,6 +20,8 @@ func dummyJenkins(){
     } else {
         dummyJenkinsLatch = true
         http.HandleFunc("/promote", promoteHandle)
+        http.HandleFunc("/lastBuild", lastBuildHandle)
+        http.HandleFunc("/jobResult", jobResultHandle)
 
         http.ListenAndServe(":7005", nil)
     }
@@ -41,13 +46,40 @@ func promoteHandle(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+//  make sure you have something that flips the jobStarted value back and forth
+func lastBuildHandle(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "GET" {
+        w.WriteHeader(405)
+        fmt.Fprintf(w, "Only accepts POST")
+    } else {
+        if !jobStarted {
+            fmt.Fprintf(w, "{\"lastBuild\":{\"actions\":[{\"parameters\":[{\"name\":\"SHIPCODE\",\"value\":\"nachoShip\"}]},{}],\"url\":\"http://localhost:7005//\"}}")
+        } else {
+            fmt.Fprintf(w, "{\"lastBuild\":{\"actions\":[{\"parameters\":[{\"name\":\"SHIPCODE\",\"value\":\"" + shipcode + "\"}]},{}],\"url\":\"http://localhost:7005//\"}}")
+        }
+    }
+}
+
+func jobResultHandle(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "GET" {
+        w.WriteHeader(405)
+        fmt.Fprintf(w, "Only accepts POST")
+    } else {
+        if !jobCompleted {
+            fmt.Fprintf(w, "{\"result\":null}")
+        } else {
+            fmt.Fprintf(w, "{\"result\":\"SUCCESS\"}")
+        }
+    }
+}
+
 func TestStartGood(t *testing.T) {
     go dummyJenkins()
 
     //  let server start
     time.Sleep(time.Second)
 
-    ciURL = "http://localhost:7005/promote"
+    ciPostURL = "http://localhost:7005/promote"
     local := &PromoteToShip{Shipcode: "local"}
     err := local.Start()
     if err != nil {
@@ -63,12 +95,42 @@ func TestStartNoShipCode(t *testing.T) {
     //  let server start
     time.Sleep(time.Second)
 
-    ciURL = "http://localhost:7005/promote"
+    ciPostURL = "http://localhost:7005/promote"
     local := &PromoteToShip{}
     err := local.Start()
     if err != nil {
         t.Logf("Correctly failed lack of shipcode with: %s", err)
     } else {
         t.Error("Succeeded when it should have failed from lack of shipcode!")
+    }
+}
+
+func TestWaitGood(t *testing.T) {
+    go dummyJenkins()
+
+    time.Sleep(time.Second)
+    ciLastBuild = "http://localhost:7005/lastBuild"
+    ciResult = "jobResult"
+
+    //  wait 5 seconds to flip from job not started to job started
+    go func() {
+        time.Sleep(5 * time.Second)
+        jobStarted = true
+    }()
+
+    //  wait 10 seconds to flip from job not complete to job completed
+    go func() {
+        time.Sleep(10 * time.Second)
+        jobCompleted = true
+    }()
+
+    shipcode = "localWait"
+    local := &PromoteToShip{Shipcode: shipcode}
+    err := local.Wait(1)
+
+    if err != nil {
+        t.Errorf("Wait call failed when it shouldn't have with: %s", err)
+    } else {
+        t.Log("Wait experiences great success!")
     }
 }
